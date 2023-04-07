3aebb4338c5f7dbaef2700392429e2cc651fdfa6
Lokoner <93461114+Lokoner@users.noreply.github.com>
2023-04-07 23:09:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,10 +6,13 @@ from scipy.stats import lognorm
 chat_id = 894208417 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    def log_likelihood(a, x):
+        n = len(x)
+        return -n*np.log(np.sqrt(2*np.pi)) - n*np.log(a) - np.sum(np.log(x**2)) - np.sum((np.log(x)-np.log(a))**2)/(2*a**2)
+    
     def neg_log_likelihood(a, x):
-        return -lognorm.logpdf(x, s=1, scale=np.exp(a)).sum()
-
-    res = minimize_scalar(neg_log_likelihood, args=(x,), method='bounded', bounds=(0, None))
-
+        return -log_likelihood(a, x)
+    
+    res = minimize_scalar(neg_log_likelihood, args=(x,), method='bounded', bounds=(1e-6, 1e6))
     return res.x
     #return x.mean() # Ваш ответ
