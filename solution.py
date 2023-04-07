03e1a44661f0239b2a9f9271948b3414fc42684f
Lokoner <93461114+Lokoner@users.noreply.github.com>
2023-04-07 22:50:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize import minimize_scalar
+from scipy.stats import lognorm
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 894208417 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    def neg_log_likelihood(a, x):
+        return -lognorm.logpdf(x, s=1, scale=np.exp(a)).sum()
+
+    res = minimize_scalar(neg_log_likelihood, args=(x,), method='bounded', bounds=(0, None))
+
+    return res.x
+    #return x.mean() # Ваш ответ
